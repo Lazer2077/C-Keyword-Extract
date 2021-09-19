6cf71ec72788336eeb8608456f49c2d60c135d0f
“Lazer2077” <709245585@qq.com>
2021-09-19 14:54:44 +0800

    level3

--- Keyword_extract.py
+++ Keyword_extract.py
@@ -2,20 +2,23 @@
 import re
 ### TOP LEVEL###
 # file_path=input("Welcome to C keyword statistic!\n Please input file Path:")
-# level=input("Please input level:")
+level=int(input("Please input level:"))
 f=open('testC.txt', 'r')
 keyword_cnt=[0]*32
 sw_cnt=[0]
 sw_icnt=-1
+if_flag=False
+if_elseif_cnt=0
+if_else_cnt=0
 shiled_word=['//','/\*','\*/','\"','\\\\n\\\\']
-annota_muti=False
-
+annota_muti=False    
 key_word=['auto','break','case','char','const', 'continue', 'default','do',
 'double','else','enum','extern','float','for','goto','if',
 'int','long','register','return','short' ,'signed' ,'sizeof','static',
 'struct','switch', 'typedef','union','unsigned','void','volatile','while']
 cnt=1
 total_num=0
+
 #print(key_word.index('case'))
 quote_pre=False
 for i in f:
@@ -24,6 +27,7 @@ for i in f:
     kwloop_cnt=0
     annota_line=-1
     quote_line=False
+    else_end=-1
     for sd in shiled_word:
         for match1 in re.finditer(sd,i):
             print(str(shloop_cnt)+"找到了！")
@@ -45,16 +49,28 @@ for i in f:
         for match in re.finditer(kw,i):
             if quote_line or annota_muti or match.start()<annota_line:
                 break
-            if kwloop_cnt==25: #find switch
-                sw_icnt+=1
-                sw_cnt.append(0)
-            if kwloop_cnt==2:   #find case
-                sw_cnt[sw_icnt]+=1
-            
+            if level>1:
+                if kwloop_cnt==25: #find switch
+                    sw_icnt+=1
+                    sw_cnt.append(0)
+                if kwloop_cnt==2:   #find case
+                    sw_cnt[sw_icnt]+=1
+            if level>2:
+                if kwloop_cnt==15:#find if
+                    if if_flag==False:
+                        if_flag=True
+                    else:
+                        if_start=match.start()
+                if kwloop_cnt==9:
+                    else_end=match.end()
             keyword_cnt[kwloop_cnt]+=1
             total_num+=1
         kwloop_cnt+=1
+    if if_flag and else_end>0:
+        if_else_cnt+=1
+        if_flag=False
     cnt+=1
+    
 f.close()
 print("total num:"+str(total_num))
 kwloop_cnt=0
@@ -67,3 +83,4 @@ for kw in key_word:
     else:
         print(kw+" num:"+str(keyword_cnt[kwloop_cnt]))
     kwloop_cnt+=1
+print("if-else num:"+str(if_else_cnt))
